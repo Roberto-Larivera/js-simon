c0a3ee63e0413a4f23a3a3336c999b94927ba46f
Roberto Larivera <robylariv@gmail.com>
2023-01-13 17:20:14 +0100

    aggiunta di setTimeout

--- js/scripts.js
+++ js/scripts.js
@@ -13,17 +13,17 @@ Consigli del giorno:
 */
 
 /*
-- visualizza in pagina 5 numeri casuali             //ok
+- visualizza in pagina 5 numeri casuali                                                         //ok
 
 - fai partine un timing di 30sec
 
 - dopo 30sec fai scomparire i numeri
 
-- richiedi all'utente i 5 numeri tramite prompt() (5 prompt())
+- richiedi all'utente i 5 numeri tramite prompt() (5 prompt())                                  //ok
 
-- dopo l'inserimento fai la verifica dei numeri da individuare sono stati azzeccati
+- dopo l'inserimento fai la verifica dei numeri da individuare sono stati azzeccati             //ok
 
-- controllo numero e ordine
+- controllo numero e ordine                                                                     //ok
 
 b - fai scegliere la difficoltà (tempo)
 b - fai scegliere quanti numeri visualizzare e in quale stadio (1-10 / 1-100 / 1-1000)
@@ -36,19 +36,26 @@ const numberUser = [];
 let totalScore = 0;
 
 controlNumberRandom (numberGuess, 1, 10)
-
 console.log('numberGuess', numberGuess);
 
-for(let i = 0; i < 5; i++){
-    const requestUser = parseInt(prompt(`Inserisci il numero ${(i +1)}`))
-    numberUser.push(requestUser)
-    if(numberUser[i] == numberGuess[i]){
-        console.log('Bravo numero indovinato')
-        totalScore++;
+setTimeout(gameUser, 5000);
+function gameUser (){
+    for(let i = 0; i < 5; i++){
+        const requestUser = parseInt(prompt(`Inserisci il numero ${(i +1)}`))
+        numberUser.push(requestUser)
+        if(numberUser[i] == numberGuess[i]){
+            console.log('Bravo numero indovinato')
+            totalScore++;
+        }
+        else{
+            console.log('Numero non indovinato')
+    
+        }
     }
+    console.log('numberUser', numberUser);
+    console.log('totalScore', totalScore);
+
 }
-console.log('numberUser', numberUser);
-console.log('totalScore', totalScore);
 
 
 //  GENERATORE NUMERI RANDOM PER BOMBA  FUNZIONE COPIATA
@@ -69,3 +76,42 @@ function controlNumberRandom (nameArray, min, max){
     }
     console.log('console log dentro funzione',nameArray.sort());
 }
+
+
+/*
+const seconds = parseInt(prompt('Inserisci il tempo di cottura:'));
+
+const risultato = trasformazioneDiSecondiWow(seconds);
+
+// TUTTE LE SEGUENTI SINTASSI SONO EQUIVALENTI
+// Sintassi 1
+setTimeout(alertTime, risultato);
+
+// Sintassi 2
+// setTimeout(alertTime, trasformazioneDiSecondiWow(seconds));
+
+// Sintassi 3
+// setTimeout(function () {
+//     alert('La pasta è pronta!');
+// }, trasformazioneDiSecondiWow(seconds));
+
+// Sintassi 4
+// setTimeout(() => {
+//     alert('La pasta è pronta!');
+// }, trasformazioneDiSecondiWow(seconds));
+
+
+
+
+function trasformazioneDiSecondiWow(inputUtente) {
+
+    return inputUtente * 1000;
+
+}
+
+function alertTime() {
+
+    alert('La pasta è pronta!');
+
+}
+*/
